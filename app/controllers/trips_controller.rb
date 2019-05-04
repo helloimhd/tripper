fa050c52a7687e88e87f497810da5a0313eadf9d
helloimhd <herda58@gmail.com>
2019-05-04 14:11:27 +0800

    edit trip form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,7 +4,7 @@ class TripsController < ApplicationController
   # GET /trips
   # GET /trips.json
   def index
-    @trips = Trip.all
+    @trips = Trip.all.where(user_id: current_user.id)
   end
 
   # GET /trips/1
