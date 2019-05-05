43e5a5ffdb6a595b52874a7114a03f7a3d958ea3
helloimhd <herda58@gmail.com>
2019-05-05 19:24:26 +0800

    request airline code

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,15 @@
+require 'open-uri'
+require 'json'
+
 class FlightsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    data = open('https://api.skypicker.com/airlines').read
+    @responses = JSON.parse(data)
+    p @responses[0][:name]
+
+  end
 end
