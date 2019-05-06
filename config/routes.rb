9958a9fa8368d6e82561178a75b45186dde5adcc
ViAyDiNe <neel.augusthy@gmail.com>
2019-05-06 11:46:51 +0800

    expenses files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,17 @@ Rails.application.routes.draw do
   resources :trips
   devise_for :users
   resources :categories
+  #resources :expenses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'main#index'
+
+  get '/trips/:id/expenses' => 'expenses#index', as: 'expenses'
+  get '/trips/:id/expenses/new' => 'expenses#new', as: 'new_expense'
+  post '/trips/:id/expenses' => 'expenses#create'
+  get '/trips/:id/expenses/:id' => 'expenses#show' , as: 'expense'
+   get '/trips/:id/expenses/:id/edit' => 'expenses#edit', as: 'edit_expense'
+  patch '/trips/:id/expenses/:id' => 'expenses#update'
+  delete '/trips/:id/expenses/:id' => 'expenses#destroy'
+
 end
