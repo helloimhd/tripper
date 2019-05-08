bdf9f994ccf6e06755aaced1d01f8b956749137e
ViAyDiNe <neel.augusthy@gmail.com>
2019-05-08 11:11:08 +0800

    finalised category chart

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,11 +7,25 @@ class ExpensesController < ApplicationController
     @spend = Expense.sum(:amount)
     @paid = Expense.where(:spent => true).sum(:amount)
     @unpaid = Expense.where(:spent => false).sum(:amount)
-    @column = @expenses.select(:category_id).group(:category_id).sum(:amount)
+    #@test = Category.all.map{|category| category.expense}.sum(:amount)
+    # byebug
+     @column = @expenses.group(:category_id).sum(:amount)
+     # p @column.keys
+     # p @column.values
+     @columnChart = {}
+     @column.keys.each_with_index do |column, index|
+      @categories.each do |category|
+        if column === category.id
+          # p category.name
+          @columnChart[category.name] = @column.values[index]
+        end
+      end
+     end
+     # p @newObjs
   end
 
   def show
-    @expense = Expense.find(params[:id])
+    @expenses = Expense.all
     @categories = Category.all
   end
 
@@ -43,6 +57,8 @@ class ExpensesController < ApplicationController
   end
 
   def destroy
+    @expense = Expense.find(params[:id])
+
     @expense.destroy
     redirect_to trips_url
   end
