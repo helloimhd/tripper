98ef313b74977a2d1bfc7351589b5b9deef50e54
helloimhd <herda58@gmail.com>
2019-05-10 14:04:33 +0800

    edit view and trip controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_05_08_063537) do
+ActiveRecord::Schema.define(version: 2019_05_10_030553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +45,6 @@ ActiveRecord::Schema.define(version: 2019_05_08_063537) do
     t.datetime "updated_at", null: false
     t.string "from"
     t.string "to"
-    t.string "airline_name"
-    t.string "from_name"
-    t.string "to_name"
     t.index ["trip_id"], name: "index_flights_on_trip_id"
   end
 
@@ -72,6 +68,7 @@ ActiveRecord::Schema.define(version: 2019_05_08_063537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "budget"
+    t.string "bg_img"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
